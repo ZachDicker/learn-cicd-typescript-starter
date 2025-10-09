333401cb5a68ce61364494dbc26390ace247d292
Zach <zach.l.dickerson@gmail.com>
2025-10-09 09:46:53 -0600

    trying to add a new workflow action

--- src/tests/api.test.ts
+++ src/tests/api.test.ts
@@ -1,20 +1,20 @@
 import { getAPIKey } from "src/api/auth";
 import { describe, expect, test } from "vitest";
 
-const validHeaders = {authorization : "ApiKey 12345"};
-const invalidHeaders = {authorization : ""}
-const APIResult : string = "12345";
+const validHeaders = { authorization: "ApiKey 12345" };
+const invalidHeaders = { authorization: "" };
+const APIResult: string = "12345";
 
 describe("getAPIKey function tests", () => {
-    test("API key is returned", () => {
-        expect(getAPIKey(validHeaders)).toBe(APIResult);
-    });
+  test("API key is returned", () => {
+    expect(getAPIKey(validHeaders)).toBe(APIResult);
+  });
 
-    test("API key is not returned", () => {
-        expect(getAPIKey(invalidHeaders)).toBe(null);
-    });
+  test("API key is not returned", () => {
+    expect(getAPIKey(invalidHeaders)).toBe(null);
+  });
 
-    test("Empty object passed to the function", () => {
-        expect(getAPIKey({})).toBeNull();
-    });
-})
+  test("Empty object passed to the function", () => {
+    expect(getAPIKey({})).toBeNull();
+  });
+});
